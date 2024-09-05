511a0ca94fcfd90bef5e2b1275fd4fdea5ac4724
Junaid shah <122867939+junaidshah786@users.noreply.github.com>
2024-09-05 12:13:51 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 # unstract_key = "c0b542d2bb6d4a6a8823bbeb753c3be6"
 # unstract_key = "0a0d712ef46045fc82d7018a3b176d34"
-unstract_key = "7290ca7ce01048b8adac57b6b7b0817e"
+unstract_key = "dbcb4a98b29c47ee92ffb2f92c7d60c6"
